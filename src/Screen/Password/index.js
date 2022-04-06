d27bbe0d18093447c8175c79f7d2e43de5d7907c
27100412duc1 <nguyenduc04121999@gmail.com>
2022-04-06 17:11:08 +0700

    Update password

--- src/Screen/Password/index.js
+++ src/Screen/Password/index.js
@@ -1,15 +1,48 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet, TextInput } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert } from "react-native";
 import BackIcon from '../../Icons/BackIcon'
 import NonIcon from '../../Icons/NonIcon'
 import SaveIcon from '../../Icons/SaveIcon'
 import { styles } from "./styles";
 import GoBack from '../../Components/GoBack'
+import { AuthContext } from "../../Redux/AuthContext";
 const UpdatePasswordScreen = (props) => {
+    const { token } = useContext(AuthContext)
+    const [currentPassword, setCurrentPassword] = useState();
+    const [password, setPassword] = useState();
+    const [password_confirmation, setPassword_confirmation] = useState();
+    const getUpDaTePassword = () => {
+        const apiURL = 'https://traderclass.vn/api/update-password';
+        fetch(apiURL, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': token.userToken.token != "" ? `Bearer ${token.userToken.token}` : ""
+            },
+            body: JSON.stringify({
+                currentPassword: currentPassword,
+                password: password,
+                password_confirmation: password_confirmation,
+            })
+        })
+            .then((res) => res.json())
+            .then((resJson) => {
+                console.log(resJson)
+                if (!resJson.status) {
+                    Alert.alert("message", resJson.msg)
+                } else {
+                    props.navigation.navigate('SiginScreen')
+                }
+            }).catch((error) => {
+                console.log('Error: ', error);
+            }).finally()
+    }
+
     return (
         <View style={{ flex: 1, backgroundColor: '#171921', padding: 15 }}>
             <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 25, justifyContent: 'space-between' }}>
-            <GoBack navigation={props.navigation}/>
+                <GoBack navigation={props.navigation} />
                 <Text style={styles.textHeader}>Change Password</Text>
                 <View style={{ paddingLeft: 6 }}><NonIcon style={styles.iconHeader} /></View>
             </View>
@@ -22,6 +55,8 @@ const UpdatePasswordScreen = (props) => {
                         Password
                     </Text>
                     <TextInput
+                        value={currentPassword}
+                        onChangeText={setCurrentPassword}
                         backgroundColor={'#272A38'}
                         style={{ height: 50 }}
                     />
@@ -31,6 +66,8 @@ const UpdatePasswordScreen = (props) => {
                         New Password
                     </Text>
                     <TextInput
+                        value={password}
+                        onChangeText={setPassword}
                         backgroundColor={'#272A38'}
                         style={{ height: 50 }}
                     />
@@ -40,12 +77,14 @@ const UpdatePasswordScreen = (props) => {
                         Re-enter Password
                     </Text>
                     <TextInput
+                        value={password_confirmation}
+                        onChangeText={setPassword_confirmation}
                         backgroundColor={'#272A38'}
                         style={{ height: 50 }}
                     />
                 </View>
                 <View style={{ marginTop: 16, marginHorizontal: 11 }}>
-                    <TouchableOpacity style={styles.touchSave}>
+                    <TouchableOpacity style={[styles.touchSave]} onPress={() => getUpDaTePassword()}>
                         <SaveIcon />
                         <Text style={styles.textTouchSave} >Save</Text>
                     </TouchableOpacity>
