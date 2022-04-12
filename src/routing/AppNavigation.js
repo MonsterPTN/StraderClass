f71a63516d77de1b80d6d9625415fcf50e49da7c
27100412duc1 <nguyenduc04121999@gmail.com>
2022-04-12 15:35:30 +0700

    Update khong luu token khi loguut 12/4

--- src/routing/AppNavigation.js
+++ src/routing/AppNavigation.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 // import { createStackNavigator } from "@react-navigation/stack";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -20,35 +20,43 @@ import CreateAccount from '../Screen/CreateAccount/CreateAccount'
 import ForgotPassword from '../Screen/ForgotPassword/ForgotPassword'
 import VerifyPhone from '../Screen/VerifyPhone/VerifyPhone'
 import VerifyOTP from '../Screen/VerifyOTP/VerityOTP'
-
+import { AuthContext } from "../Redux/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
-export default function AppNavigation () {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator
-                    screenOptions={{ headerShown: false }}
-                    initialRouteName={'StartScreen'}>
-                    <Stack.Screen name="StartScreen" component={StartScreen} />
-                    <Stack.Screen name="SiginScreen" component={SiginScreen} />
-                    <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-                    <Stack.Screen name="CreateAccount" component={CreateAccount} />
-                    <Stack.Screen name="VerifyPhone" component={VerifyPhone} />
-                    <Stack.Screen name="VerifyOTP" component={VerifyOTP} />
-                    <Stack.Screen name="MainTabScreen" component={MainTabScreen} />
-                    <Stack.Screen name="TestScreen" component={TestScreen} />
-                    <Stack.Screen name="User" component={User} />
-                    <Stack.Screen name="Profile" component={Profile} />
-                    <Stack.Screen name="Membership" component={Membership} />
-                    <Stack.Screen name="Referall" component={Referall} />
-                    <Stack.Screen name="UpdateEmail" component={UpdateEmail} />
-                    <Stack.Screen name="Password" component={Password} />
-                    <Stack.Screen name="Terms" component={Terms} />
-                    <Stack.Screen name="Lesturers" component={Lesturers} />
-                    <Stack.Screen name="Watch" component={Watch} />
-                    <Stack.Screen name="Payment" component={Payment} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        )
+export default function AppNavigation() {
+    const { token } = useContext(AuthContext)
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{ headerShown: false }} >
+                {token.userToken == "" ?
+                    <>
+                        <Stack.Screen name="StartScreen" component={StartScreen} />
+                        <Stack.Screen name="SiginScreen" component={SiginScreen} />
+                        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+                        <Stack.Screen name="CreateAccount" component={CreateAccount} />
+                        <Stack.Screen name="VerifyPhone" component={VerifyPhone} />
+                        <Stack.Screen name="VerifyOTP" component={VerifyOTP} />
+                    </>
+                    :
+                    <>
+                        <Stack.Screen name="MainTabScreen" component={MainTabScreen} />
+                        <Stack.Screen name="TestScreen" component={TestScreen} />
+                        <Stack.Screen name="User" component={User} />
+                        <Stack.Screen name="Profile" component={Profile} />
+                        <Stack.Screen name="Membership" component={Membership} />
+                        <Stack.Screen name="Referall" component={Referall} />
+                        <Stack.Screen name="UpdateEmail" component={UpdateEmail} />
+                        <Stack.Screen name="Password" component={Password} />
+                        <Stack.Screen name="Terms" component={Terms} />
+                        <Stack.Screen name="Lesturers" component={Lesturers} />
+                        <Stack.Screen name="Watch" component={Watch} />
+                        <Stack.Screen name="Payment" component={Payment} />
+                    </>
+                }
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
