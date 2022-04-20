d4fb25ce95f82ee1d7c0a9465a6b41f881f30ec0
dangpham3040 <dangphamk18@gmail.com>
2022-04-20 10:54:42 +0700

    fix

--- src/Screen/Start/StartScreen.js
+++ src/Screen/Start/StartScreen.js
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { styles } from './styles'
 
 import {
@@ -11,7 +10,8 @@ import {
   useColorScheme,
   View,
   TouchableOpacity,
-  ImageBackground
+  ImageBackground,
+  Alert
 } from 'react-native';
 
 import AppleIcon from '../../Icons/AppleIcon/AppleIcon';
@@ -19,9 +19,11 @@ import GoogleIcon from '../../Icons/GoogleIcon/GoogleIcon';
 import FaceBookIcon from '../../Icons/FacebookIcon/FaceBookIcon';
 import { scale } from 'react-native-size-matters';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { AuthContext } from '../../Redux/AuthContext';
 import auth from '@react-native-firebase/auth';
 
 export default function App({ navigation }) {
+  const { setToken } = useContext(AuthContext)
   GoogleSignin.configure({
     webClientId: '415484737583-h9r46fnd6sagfhrrq5nje7coeasjtn8t.apps.googleusercontent.com',
   });
@@ -33,11 +35,42 @@ export default function App({ navigation }) {
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
   
     // Sign-in the user with the credential
-    const user =  auth().signInWithCredential(googleCredential);
-    
-    user.then((u)=>{
-      console.log(u)
+    const profile = auth().signInWithCredential(googleCredential);
+    profile.then((u) => {
+      const user = u.user
+      getLogin(user.displayName, user.email, user.photoURL, user.phoneNumber, 1)
+     
     })
+  }
+  const getLogin = async (name, email, photo, phone, auth_type) => {
+    const apiURL = 'https://traderclass.vn/api/login-social';
+    fetch(apiURL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        photo: photo,
+        phone: phone,
+        auth_type : auth_type 
+      }),
+    })
+      .then((res) => res.json())
+      .then((resJson) => {
+        if (!resJson.status) {
+          Alert.alert("message", resJson.msg)
+        } else {
+          setToken({ loading: false, userToken: resJson.data })
+          navigation.navigate('MainTabScreen')
+          Alert.alert("message", resJson.msg)
+        }
+
+      }).catch((error) => {
+        console.log('Error: ', error);
+      })
   }
   return (
 
